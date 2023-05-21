c9423232452d3164c2a0b946c9506f1a18f9c7cb
IlaydaSurkultay <127659973+IlaydaSurkultay@users.noreply.github.com>
2023-05-21 03:26:04 +0300

    AppCs + AppJs + component/Navbar.jsx eklemeler; assets + component/Home.jsx + component/Products.jsx olusturuldu

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,34 +3,38 @@ import React from "react";
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container">
-                    <a class="navbar-brand fw-bold fs-4" href="#">
+            <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+                <div className="container">
+                    <a className="navbar-brand fw-bold fs-4" href="#">
                         CS391
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Products</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Products</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contact</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact</a>
                             </li>
                            
                         </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                        <div className="buttons">
+                           <a href="" className="btn btn-outline-dark">
+                            <i className="fa fa-sign-in me-1"></i> Login</a>
+                            <a href="" className="btn btn-outline-dark">
+                            <i className="fa fa-user-plus me-1"></i> Register</a>
+                            <a href="" className="btn btn-outline-dark">
+                            <i className="fa fa-shopping-cart me-2"></i> Cart (0)</a>
+                        </div>
                     </div>
                 </div>
             </nav>
